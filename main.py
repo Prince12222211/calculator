320af7015f44964b02ff6d523c8a1f9778618e88
princeverma123 <114815472+princeverma123@users.noreply.github.com>
2022-10-01 18:59:55 +0530

    hacktoberfest-2022
    I hope you will help me to improve this code i am beginner in coding

---
+++ main.py
@@ -0,0 +1,21 @@
+calculate=input("What do you want to calculate:-")
+if calculate =="add":
+	a=input("first number:=")
+	b=input("\nsecond number:=")
+	c=float(a)+float(b)
+
+	print("the sum of numbers=",format(c))
+elif  calculate=="sub":
+	a=input("first number:=")
+	b=input("\nsecond number:=")
+	c=float(a)-float(b)
+
+	print("the sum of numbers=",format(c))
+elif calculate=="mul":
+	a=input("first number:=")
+	b=input("\nsecond number:=")
+	c=float(a)*float(b)
+
+	print("the sum of numbers=",format(c))
+
+# C:\Users\princ\PycharmProjects\pythonProject\venv
